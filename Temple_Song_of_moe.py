6b17af4a8ce3e2d5113110c0e058be53b99b2e76
GEYUANwuqi <17539198883@163.com>
2024-04-04 11:49:10 +0800

    modified:   Temple_Song_of_moe.py

--- Temple_Song_of_moe.py
+++ Temple_Song_of_moe.py
@@ -1,8 +1,12 @@
+from pip._internal import main
 import asyncio
 import datetime
-from bilibili_api import video
 
-print("使用办法：\n输入视频的bv号,将自动输出符合规范的Temple Song模板\n正常打卡文件会输出类似“D:/xxx/xxxxx/Temple_Song_of_moe”的代码,这是正常情况\n")
+print(f"使用办法：\n输入视频的bv号,将自动输出符合规范的Temple Song模板\n正常打开文件并检查更新完成后会输出类似“D:/xxx/xxxxx/文件名”的代码,这是正常情况\n该工具最新的更新地址在“https://zh.moegirl.org.cn/User:%E4%BC%8D%E6%98%B1%E7%89%A9%E8%B5%B7/tsbpy”\n")
+print(f"自动检查更新中...\n(此过程大概需要10-20s)\n")
+main(['install', 'bilibili_api'])
+print(f"\n检查已完成\n若出现导致程序非正常运行的错误请检查错误信息\n(此工具大部分的错误信息都不影响程序正常运行)\n")
+from bilibili_api import video
 
 async def main() -> None:
     v = video.Video(bvid=input("\n请输入bv号:"))# 实例化 Video 类
@@ -29,7 +33,7 @@ async def main() -> None:
     #模板
 
     print(f"\n{left}{times}\n{bbid}{bv}\n{song}{title}\n{btime}{time}\n{bcount}{left}{bilicount}{bv}{right}\n{image}{pic}\n{right}\n")
-    input("Ctrl+C复制后按enter退出！")
+    input("Ctrl+C复制后按enter退出...")
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(main())
